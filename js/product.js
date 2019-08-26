ddc8308795f6078140e46847b23e2ba03c281c88
lee <hn85412404@gmail.com>
2019-08-26 17:51:09 +0800

    [modify] 商品試算

--- js/product.js
+++ js/product.js
@@ -5,54 +5,59 @@ var Product = {
 			el: '#app',
 			data: {
 				productPrice: 0,
-                shipping: 0,
-                currentRate: 33,
-            },
-            watch: {
-                productPrice: function(){
-                    if( this.productPrice < 0 ) this.productPrice = 0;
-                },
-                shipping: function(){
-                    if( this.shipping < 0 ) this.shipping = 0;
-                }
-            },
-            created () {
-                const _this = this;
-
-                axios.get('https://api.coinbase.com/v2/exchange-rates?currency=USD')
-                    .then(function (response) {
-                        const { data: { data : { rates: { TWD }}} } = response;
-                        console.log(TWD);
-                        _this.currentRate = TWD;
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
-            },
-			computed: {
-                currentFee: function() {
-                    let result1 =  Math.floor(this.currentRate*100)/100 ;
-                    let result2 =  Math.floor((result1+0.25)*2)/2 + 0.5;
-                    return result2
-                },
-                cost: function(){
-                    console.log(this.productPrice, this.shipping);
-                    return Number(this.productPrice) +  Number(this.shipping);
-                },
-                totalCost: function() {
-                    return this.cost* Number(this.currentFee)
-                },
-				resultHtml: function() {
-					return `NT$${this.cost} x ${this.currentFee}(當前匯率)x1 (免代買費)=<br> NT$<span>${this.totalCost}</span>元整 + 國際運費`;
+				shipping: 0,
+				currentRate: 33,
+			},
+			watch: {
+				productPrice: function() {
+					if (this.productPrice < 0) this.productPrice = 0;
 				},
-				
+				shipping: function() {
+					if (this.shipping < 0) this.shipping = 0;
+				},
+			},
+			created: function() {
+				axios
+					.get('https://api.coinbase.com/v2/exchange-rates?currency=USD')
+					.then(function(response) {
+						const {
+							data: {
+								data: {
+									rates: { TWD },
+								},
+							},
+						} = response;
+						Product.instanceVue.currentRate = TWD;
+					})
+					.catch(function(error) {
+						console.log(error);
+					});
+			},
+			computed: {
+				currentFee: function() {
+					let result1 = Math.floor(this.currentRate * 100) / 100;
+					let result2 = Math.floor((result1 + 0.25) * 2) / 2 + 0.5;
+					return result2;
+				},
+				cost: function() {
+					console.log(this.productPrice, this.shipping);
+					return Number(this.productPrice) + Number(this.shipping);
+				},
+				totalCost: function() {
+					return this.cost * Number(this.currentFee);
+				},
+				resultHtml: function() {
+					return `$${this.cost} x ${
+						this.currentFee
+					}(當前匯率)x1 (免代買費)=<br> NT$<span>${
+						this.totalCost
+					}</span>元整 + 國際運費`;
+				},
 			},
 			mounted: function() {
-                // console.log(this.currentRate ,this.currentFee)
-            },
-			methods: {
-				
+				// console.log(this.currentRate ,this.currentFee)
 			},
+			methods: {},
 		});
 	},
 };
