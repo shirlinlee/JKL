5fbe4b309d0c3337f00a1937fcd3fa7bfe545147
Unknown <hn85412404@gmail.com>
2019-09-30 00:27:02 +0800

    Auto stash before merge of "master" and "origin/master"

--- js/shipping.js
+++ js/shipping.js
@@ -1,6 +1,6 @@
 var Shipping = {
 	instanceVue: undefined,
-	Setup: function() {
+	Setup() {
 		Shipping.instanceVue = new Vue({
 			el: '#app',
 			data: {
@@ -18,10 +18,10 @@ var Shipping = {
 				productPrice: 0,
 				shipping: 0,
 				currentRate: 33,
-				buyingFee: 1,
+				// buyingFee: 1,
 			},
 			computed: {
-				computedVolume: function() {
+				computedVolume() {
 					// 計算 體積磅數
 					const rateVolume = this.unitVolume[this.selectedVolume].rate;
 					const length = Math.ceil(this.length);
@@ -32,21 +32,27 @@ var Shipping = {
 					);
 					return result;
 				},
-				computedWeight: function() {
+				computedWeight() {
 					// 計算 磅數
 					const rateWeight = this.unitWeight[this.selectedWeight].rate;
 					const weight = Math.ceil(this.weight);
 					const result = Math.ceil(weight * rateWeight);
 					return result;
 				},
-				computedShippingPound: function() {
+				computedShippingPound() {
 					// 體積磅數 與 重量磅數 兩者的取最大值
+					console.log(Math.max(
+						this.selectedPlan === 0 ? 0 : this.computedVolume || 0,
+						this.computedWeight
+					))
 					return Math.max(
 						this.selectedPlan === 0 ? 0 : this.computedVolume || 0,
 						this.computedWeight
 					);
+					
+
 				},
-				computedShippingCost: function() {
+				computedShippingCost() {
 					// 當前對應的方案
 					const plan = this.shippingPlan[this.selectedPlan];
 
@@ -60,57 +66,60 @@ var Shipping = {
 						? this.computedShippingPound * plan.shipping.limitRatio
 						: plan.shipping.pricing[this.computedShippingPound];
 
+					// console.log(this.computedShippingPound <= plan.shipping.maximumPound
+					// 	?  this.toTWLocalePrice(shipping)
+					// 	: '請來信洽詢')
 					return this.computedShippingPound <= plan.shipping.maximumPound
 						?  this.toTWLocalePrice(shipping)
 						: '請來信洽詢';
 				},
-				currentFee: function() {
+				currentFee() {
 					let result1 = Math.floor(this.currentRate * 100) / 100;
 					let result2 = Math.floor((result1 + 0.25) * 2) / 2 + 0.5;
 					return result2;
 				},
-				cost: function() {
-					return Number(this.productPrice) + Number(this.shipping);
-				},
-				totalCost: function() {
-					// return this.cost * Number(this.currentFee);
-					// if()
-					console.log(typeof this.computedShippingCost);
-
-					return Number(this.computedShippingCost) + this.buyingFeeNumber;
-				},
-				// resultHtml: function() {
-				// 	if( Number(this.productPrice) !== '') {
-
-				// 		return `$ <font class='f_blue f_b f24'>${this.cost} </font> x ${
-				// 			this.currentFee
-				// 		}(當前匯率) x 1 (免代買費)=<br> NT$ <font class="f_blue f_b f24">${
-				// 			this.totalCost
-				// 		}</font> 元整 + 國際運費`;
-				// 	}
-				// 	return `<span class="f_grey">請填寫商品總價</span>    `
+				// cost() {
+				// 	return Number(this.productPrice) + Number(this.shipping);
 				// },
-				buyingFeeNumber: function() {
-					console.log(this.buyingFee, this.productPrice, this.currentRate)
-					return this.buyingFee * this.productPrice * this.currentRate;
+				totalCost() {
+					// console.log(Number(this.computedShippingCost.replace(/[^0-9.-]+/g,"")), this.computedShippingCost);
+					return Math.floor(Number(this.computedShippingCost.replace(/[^0-9.-]+/g,""))) + Number(this.buyingFeeNumber.replace('NT ',''));
+				},
+				buyingFee() {
+				
+					if ( this.productPrice < 175 ) {
+						return 300;
+					} else if ( this.productPrice >= 175 && this.productPrice < 501 ) {
+						return 1.03;
+					} else if ( this.productPrice >= 501 && this.productPrice < 1001 ) {
+						return 1.01;
+					}
+					return  1
+				},
+				buyingFeeNumber() {
+					
+					if ( this.productPrice < 175 ) {
+						return 'NT '+ Math.floor( this.productPrice * this.currentFee + this.buyingFee );
+					}
+					return 'NT '+Math.floor(this.productPrice * this.currentFee * this.buyingFee);
 				}
 			},
 			watch: {
-				computedShippingPound: function(newValue) {
+				computedShippingPound(newValue) {
 					// 當前對應的方案
 					const plan = this.shippingPlan[this.selectedPlan];
 					if (newValue > plan.shipping.maximumPound) {
 						alert('請來信洽詢');
 					}
 				},
-				productPrice: function() {
+				productPrice() {
 					if (this.productPrice < 0) this.productPrice = 0;
 				},
-				shipping: function() {
+				shipping() {
 					if (this.shipping < 0) this.shipping = 0;
 				},
 			},
-			created: function() {
+			created() {
 				if(window.location.hash === '#notitle') this.noTitle = true;
 				
 				axios
@@ -123,17 +132,17 @@ var Shipping = {
 								},
 							},
 						} = response;
-						Product.instanceVue.currentRate = TWD;
+						Shipping.instanceVue.currentRate = TWD;
 					})
 					.catch(function(error) {
 						console.log(error);
 					});
 			},
-			mounted: function() {
+			mounted() {
 
 			},
 			methods: {
-				toTWLocalePrice: function(price) {
+				toTWLocalePrice(price) {
 					return parseInt(price, 10).toLocaleString('zh-TW', {
 						currency: 'TWD',
 					});
